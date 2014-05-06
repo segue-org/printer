6137306469ba0d70300d0fd6a84862a94abd5883
Felipe Mobus <fmobus@gmail.com>
2014-05-06 14:54:43 -0300

    added press mode

---
+++ press.py
@@ -0,0 +1,47 @@
+import sys
+import codecs
+
+from lib.badge import Badge
+
+sys.stdout = codecs.open('/dev/stdout', 'w', 'utf8')
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print "USAGE: cat FILE | python expo.py <PREFIX> <SMALLTEXT> <STARTON>"
+    sys.exit(1);
+
+  prefix  = sys.argv[1];
+  small   = sys.argv[2]
+  starton = int(sys.argv[3])
+
+  current = starton
+
+  print 'AGUARDANDO ENTRADA...';
+  while True:
+    print "FORMATO: name,corp<enter>";
+    line = sys.stdin.readline()
+    if len(line.strip()) == 0:
+      print '*** empty, quitting'
+      break;
+    if line.startswith("#"):
+      print "*** pulando " + line;
+      continue;
+    if line.strip() == "exit":
+      print "*** exit";
+      break;
+
+    line = line.decode('utf-8')
+    print line
+    if line.strip() == 'repeat':
+      text, name = last;
+    else:
+      items = line.strip().split(',')
+      print items
+      text, name = items
+      last = text, name
+
+    xid = "{}-{}".format(prefix, current)
+    current += 1
+    print xid, name, text, small
+    # Badge(xid, name, text, small)
+
