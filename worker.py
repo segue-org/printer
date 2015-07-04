d4ea5c9db48e123add02cb0d1e6b5a51c3f69f52
Felipe Mobus <fmobus@gmail.com>
2015-07-04 11:49:18 -0300

    added construction of prefixed xids

--- worker.py
+++ worker.py
@@ -1,8 +1,20 @@
 from lib.badge import Badge
 from lib.envelope import Envelope
 
+PREFIXES = {
+    'speaker':           'P',
+    'proponent':         'N',
+    'normal':            'N',
+    'promocode':         'N',
+    'student':           'S',
+    'caravan':           'N',
+    'caravan-leader':    'N',
+    'proponent-student': 'S'
+}
+
 def print_badge(value):
-    xid = value['xid']
+    prefix = PREFIXES.get(value.get('category','normal'),'X')
+    xid = "{}-{}".format(prefix, value.get('xid', 0))
     name = value.get('name',None)
     company = value.get('organization',None)
     city = value.get('city', None)
