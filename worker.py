496e8e54a4799af8825562fcf54e5d272b7bdfe2
Felipe Mobus <fmobus@gmail.com>
2015-07-04 11:17:46 -0300

    fixing imports in workeer

--- worker.py
+++ worker.py
@@ -1,3 +1,6 @@
+from lib.badge import Badge
+from lib.envelope import Envelope
+
 def print_badge(value):
     xid = value['xid']
     name = value.get('name',None)
@@ -5,5 +8,5 @@ def print_badge(value):
     city = value.get('city', None)
     copies = value.get('copies', 1)
     badge    = Badge(xid, name, company, city, copies)
-    envelope = Badge(xid, name, company, city, 1)
+    envelope = Envelope(xid, name, company, city, 1)
     return badge;
